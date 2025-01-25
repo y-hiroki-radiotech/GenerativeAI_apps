7d146f9fa83b1226644d8abfcf628a82df87ddde
y-hiroki-radiotech <bnndd439@ybb.ne.jp>
2025-01-26 08:10:33 +0900

    finance_agent finish

---
+++ 12.Building_Agentic_AI_Course/my_financial_agent.py
@@ -0,0 +1,44 @@
+from dotenv import load_dotenv
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.duckduckgo import DuckDuckGo
+from phi.tools.yfinance import YFinanceTools
+
+load_dotenv()
+
+# *Web search agent
+web_search_agent = Agent(
+    name="Web Search Agent",
+    role="Search the web for information",
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[DuckDuckGo()],
+    instructions=["Always include sources"],
+    show_tools_calls=True,
+    markdown=True,
+)
+
+
+# *Financial agent
+finance_agent = Agent(
+    name="Finance AI Agent",
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[
+        YFinanceTools(
+            stock_price=True, analyst_recommendations=True, stock_fundamentals=True
+        )
+    ],
+    instructions=["Use tables to display the data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+multi_ai_agent = Agent(
+    team=[web_search_agent, finance_agent],
+    instructions=["Always include sources", "Use table to display the data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+multi_ai_agent.print_response(
+    "Summarize analyst recommendation and share the latest news for NVIDIA", stream=True
+)
